431311a4741660a87759e535575b849f80f65e48
Bence <sifo996@gmail.com>
2022-04-17 19:05:33 +0200

    secret-retriever service created for retrieving SERVERACCESS

---
+++ infra/secret-retriever.service.ts
@@ -0,0 +1,47 @@
+
+import * as AWS from 'aws-sdk'
+import * as fs from 'fs'
+
+const client = new AWS.SecretsManager({
+    region: 'eu-west-2',
+});
+
+
+
+function getAwsSecretValueFromClient(secretName: string) {
+    return client.getSecretValue({ SecretId: secretName }).promise();
+}
+
+async function getAwsSecretAsync(secretName: string) {
+    try {
+        const response = await getAwsSecretValueFromClient(secretName);
+        return response;
+    } catch (error) {
+        console.error('Error occurred while retrieving AWS secret');
+        console.error(error);
+        return null;
+    }
+}
+
+function writeSecretIntoFile(secretObject: any) {
+    if (secretObject) {
+        const file = fs.createWriteStream("../app/.htpasswd")
+
+        secretObject.forEach((element: any) => {
+            file.write(`${element}\n`)
+        });
+
+        file.close()
+    }
+}
+
+export async function getSecretAndWriteFile(secretName?: string) {
+    //process.env.SERVERACCESS
+    if (secretName) {
+        const secret = await getAwsSecretAsync(secretName);
+        if (secret?.SecretString) {
+            var secretObject = JSON.parse(secret.SecretString)["SERVERACCESS"].split(",");
+            writeSecretIntoFile(secretObject)
+        }
+    }
+}
