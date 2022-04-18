96bf1754124b72de6667b445dc4a55d298bc94d3
Bence <sifo996@gmail.com>
2022-04-18 11:55:33 +0200

    secret-retriever service updated

--- infra/secret-retriever.service.ts
+++ infra/secret-retriever.service.ts
@@ -1,13 +1,6 @@
 
-import * as AWS from 'aws-sdk'
 import * as fs from 'fs'
-
 import * as aws from "@pulumi/aws";
-import * as pulumi from "@pulumi/pulumi";
-
-const client = new AWS.SecretsManager({
-    region: process.env.AWS_REGION || "eu-west-2",
-});
 
 function getAwsSecretValueFromClient(secretName: string) {
     return aws.secretsmanager.getSecretVersion({ secretId: secretName });
@@ -39,7 +32,6 @@ function writeSecretIntoFile(secretObject: any) {
 export async function getSecretAndWriteFile(secretName?: string) {
     if (secretName) {
         const secret = await getAwsSecretAsync(secretName);
-        console.log('this is a secret', secret)
         if (secret?.secretString) {
             var secretObject = JSON.parse(secret.secretString)["SERVERACCESS"].split(",");
             writeSecretIntoFile(secretObject)
